211acfc8cb6e7cd6c4fda965c62b0d2315205e73
Georg Graf (SPG) <grafg@spengergasse.at>
2025-12-10 03:55:08 +0100

    add file to db

--- files/usr_local_opnsense_scripts_custom_nmap_cron.sh
+++ files/usr_local_opnsense_scripts_custom_nmap_cron.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 logdir=/var/log/exampy
 filename=$(date "+%Y-%m-%d_%H:%M:%S").log
-/usr/local/bin/nmap -sn -n -PR -oG - 192.168.54.0/24 > ${logdir}/${filename}
+/usr/local/bin/nmap -sn -n -PR -oG ${logdir}/${filename} 192.168.21.0/24 
+/usr/local/bin/curl -X POST http://localhost:8000/newscanfile -H "Content-Type: application/json" -d '{"file": "'${filename}'"}'
