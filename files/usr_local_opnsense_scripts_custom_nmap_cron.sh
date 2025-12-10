70d5e3158f9b804ab08f2e13743b29baa63915a9
Georg Graf (SPG) <grafg@spengergasse.at>
2025-12-10 03:56:31 +0100

    nmap ..

--- files/usr_local_opnsense_scripts_custom_nmap_cron.sh
+++ files/usr_local_opnsense_scripts_custom_nmap_cron.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 logdir=/var/log/exampy
 filename=$(date "+%Y-%m-%d_%H:%M:%S").log
-/usr/local/bin/nmap -sn -n -PR -oG ${logdir}/${filename} 192.168.21.0/24 
+/usr/local/bin/nmap -sn -n -PO -PE -PP -PM -PR -oG ${logdir}/${filename} 192.168.21.0/24 
 /usr/local/bin/curl -X POST http://localhost:8000/newscanfile -H "Content-Type: application/json" -d '{"file": "'${filename}'"}'
