046d9f9e3df8fa04f4dff43338337893bc859d85
Georg Graf (SPG) <grafg@spengergasse.at>
2025-12-10 04:23:49 +0100

    log cron ;)

--- files/usr_local_opnsense_scripts_custom_nmap_cron.sh
+++ files/usr_local_opnsense_scripts_custom_nmap_cron.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 logdir=/var/log/exampy
 filename=$(date "+%Y-%m-%d_%H:%M:%S").log
+exec 1>>${logdir}/nmap_cron.log 2>&1
 /usr/local/bin/nmap -sn -n -PO -PE -PP -PM -PR -oG ${logdir}/${filename} 192.168.21.0/24 
 /usr/local/bin/curl -X POST http://localhost:8000/newscanfile -H "Content-Type: application/json" -d '{"file": "'${filename}'"}'
