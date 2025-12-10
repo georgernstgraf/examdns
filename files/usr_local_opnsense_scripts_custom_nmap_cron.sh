5ac7a1eb982f834ddd7e467da3a6a1f0faad97d1
Georg Graf (SPG) <grafg@spengergasse.at>
2025-12-10 04:38:37 +0100

    more loggin

--- files/usr_local_opnsense_scripts_custom_nmap_cron.sh
+++ files/usr_local_opnsense_scripts_custom_nmap_cron.sh
@@ -3,4 +3,5 @@ logdir=/var/log/exampy
 filename=$(date "+%Y-%m-%d_%H:%M:%S").log
 exec 1>>${logdir}/nmap_cron.log 2>&1
 /usr/local/bin/nmap -sn -n -PO -PE -PP -PM -PR -oG ${logdir}/${filename} 192.168.21.0/24 > /dev/null
+echo "nmap exited with code $?"
 /usr/local/bin/curl --silent -X POST http://localhost:8000/newscanfile -H "Content-Type: application/json" -d '{"file": "'${filename}'"}' | /usr/local/bin/jq
