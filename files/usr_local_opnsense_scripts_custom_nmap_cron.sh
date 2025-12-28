5dddccbff353df98594b43d16b9b0aac6bb02508
Georg Graf (sense root) <grafg@spengergasse.at>
2025-12-28 22:36:00 +0100

    scan_with_pf

--- files/usr_local_opnsense_scripts_custom_nmap_cron.sh
+++ files/usr_local_opnsense_scripts_custom_nmap_cron.sh
@@ -2,6 +2,4 @@
 logdir=/var/log/exampy
 filename=$(date "+%Y-%m-%d_%H:%M:%S").log
 exec 1>>${logdir}/scan_cron.log 2>&1
-/root/bin/scan.py > ${logdir}/${filename}
-echo "scan exited with code $? - stderr (if any) is above, now eat file"
-/usr/local/bin/curl --silent -X POST http://192.168.21.1:8000/newscanfile -H "Content-Type: application/json" -d '{"file": "'${filename}'"}' | /usr/local/bin/jq
+/root/bin/scan.py
